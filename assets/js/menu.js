2def30643163ed92af309c757a38cdab0cd2b363
ajishtmohan <41186016+ajishtmohan@users.noreply.github.com>
2022-05-25 00:37:45 +0530

    update
    Vision & Mission
    Industry Interface

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -47,3 +47,146 @@ window.onscroll = function () {
   }
   prevScrollpos = currentScrollPos;
 };
+
+let siteLinks = document.querySelector('.site-links');
+console.log(siteLinks);
+siteLinks.innerHTML = `<div class="navbar">
+<div class="logo">
+<a href="index.html">
+    <img class="logo__img" src="assets/img/sse-1.png" alt="" />
+  </a>
+</div>
+<nav class="navigation">
+  <ul class="navigation__list">
+    <li class="navigation__item"><a class="navigation__item--link" href="index.html">Home</a></li>
+    <li class="navigation__item">
+      <a class="navigation__item--link" href="#">About Us ↓</a>
+
+      <ul class="submenu">
+        <li class="submenu__item linked">
+          <a href="our-heritage.html">Our&nbsp;Heritage </a>
+        </li>
+        <li class="submenu__item linked">
+          <a href="#">Vision&nbsp;&&nbsp;Mission</a>
+        </li>
+        <li class="submenu__item linked">
+          <a href="chairman-message.html">Chairman&nbsp;Message</a>
+        </li>
+        <li class="submenu__item linked"><a href="dean-message.html">Dean&nbsp;Message</a></li>
+        <li class="submenu__item linked">
+          <a href="principal-message.html">Principal&nbsp;Message</a>
+        </li>
+        <li class="submenu__item linked">
+          <a href="industry-interface.html">Industry&nbsp;Interface</a>
+        </li>
+        <li class="submenu__item">
+          <a href="#">Affiliation&nbsp;&&nbsp;Recognition </a>
+        </li>
+        <li class="submenu__item">
+          <a href="#">Governing&nbsp;Body</a>
+        </li>
+        <li class="submenu__item"><a href="#">RISE</a></li>
+      </ul>
+    </li>
+
+    <li class="navigation__item">
+      <a class="navigation__item--link" href="#">Academics ↓</a>
+      <ul class="submenu">
+        <li class="submenu__item linked">
+          <a href="courses.html">Courses »</a>
+          <ul class="submenu-2">
+            <li class="linked">
+              <a href="cse.html"
+                >Computer Science & Engineering (SOSO)</a
+              >
+            </li>
+            <li>
+              <a href="#"
+                >Electronics & Communication Engineering (SLICE)</a
+              >
+            </li>
+            <li>
+              <a href="#">Electrical & Electronics Engineering (PPP)</a>
+            </li>
+            <li>
+              <a href="#">Mechanical Engineering (SMILE)</a>
+            </li>
+            <li>
+              <a href="#">Civil Engineering (FORCE)</a>
+            </li>
+            <li>
+              <a href="#">Humanities and Science</a>
+            </li>
+          </ul>
+        </li>
+        <li class="submenu__item">
+          <a href="#">Faculty</a>
+        </li>
+        <li class="submenu__item">
+          <a href="#">Value&nbsp;Added&nbsp;Courses</a>
+        </li>
+        <li class="submenu__item"><a href="#">Examinations</a></li>
+        <li class="submenu__item">
+          <a href="#">Syllabus</a>
+        </li>
+        <li class="submenu__item">
+          <a href="#">E-Resources</a>
+        </li>
+      </ul>
+    </li>
+
+    <li class="navigation__item linked">
+      <a class="navigation__item--link" href="placements.html">Placements</a>
+      <ul class="submenu">
+        <li class="submenu__item">
+          <a href="#">Vision&nbsp;&&nbsp;Mission</a>
+        </li>
+        <li class="submenu__item">
+          <a href="#">Modus&nbsp;Operandi</a>
+        </li>
+        <li class="submenu__item">
+          <a href="#">Placements&nbsp;Team</a>
+        </li>
+        <li class="submenu__item">
+          <a href="#">Our&nbsp;Recruiters</a>
+        </li>
+        <li class="submenu__item">
+          <a href="#">Recruitment&nbsp;details</a>
+        </li>
+      </ul>
+    </li>
+
+    <li class="navigation__item">
+      <a class="navigation__item--link" href="#">IQAC ↓</a>
+      <ul class="submenu">
+        <li class="submenu__item">
+          <a href="#">Members</a>
+        </li>
+        <li class="submenu__item">
+          <a href="#">SSE&nbsp;Best&nbsp;Practices</a>
+        </li>
+        <li class="submenu__item">
+          <a href="#">Annual&nbsp;Reports</a>
+        </li>
+      </ul>
+    </li>
+
+    </li>
+    <li class="navigation__item">
+      <a class="navigation__item--link" href="#">Committies</a>
+    </li>
+    <li class="navigation__item">
+      <a class="navigation__item--link" href="#">Facilities</a>
+    </li>
+    <li class="navigation__item">
+      <a class="navigation__item--link" href="#">SSE Media</a>
+    </li>
+    <li class="navigation__item">
+      <a class="navigation__item--link" href="#">Mandatories</a>
+    </li>
+    <li class="navigation__item">
+      <a class="navigation__item--link" href="#">Alumni</a>
+    </li>
+  </ul>
+</nav>
+</div>`;
