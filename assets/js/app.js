e4d7492950a22c98f42fbdc1b564164a5eb3c63f
ajishtmohan <41186016+ajishtmohan@users.noreply.github.com>
2022-07-03 22:12:39 +0530

    complete!!!

--- assets/js/app.js
+++ assets/js/app.js
@@ -151,7 +151,6 @@ const window375to600 = function () {
   const quicklinks = document.querySelector('.quicklinks');
   const siteMenu = document.querySelector('.site-menu');
   const nav = document.querySelector('.nav');
-  const mobileMenu = document.querySelector('.mobile-naav');
 
   menuCheck.addEventListener('change', function () {
     if (menuCheck.checked) {
@@ -161,7 +160,6 @@ const window375to600 = function () {
         quickMenu.style.display = 'none';
         siteMenu.style.display = 'inline-block';
         nav.style.padding = '15rem 5%';
-        mobileMenu.style.display = 'none';
       }, 300);
     } else {
       quicklinks.style.display = 'none';
@@ -176,13 +174,22 @@ const window375to600 = function () {
 };
 
 // MENU BUTTON ANIMATION
+const menuCheck = document.getElementById('nav-toggle');
+menuCheck.addEventListener('change', function () {
+  if (menuCheck.checked) {
+    burgerButtonOpen();
+  } else {
+    burgerButtonClose();
+  }
+});
 
 const burgerButtonOpen = function () {
   const burgerLines = document.querySelectorAll('.icon-burger__line');
+
   console.log(burgerLines);
   for (let i = 0; i < burgerLines.length; i++) {
     burgerLines[i].style.cssText =
-      'display: fixed; position: absolute; right: 5%; top: 50%;';
+      'display: block; position: absolute; right: 5%;';
     burgerLines[0].style.transform = 'rotate(45deg)';
     burgerLines[1].style.display = 'none';
     burgerLines[2].style.transform = 'rotate(-45deg)';
@@ -191,6 +198,7 @@ const burgerButtonOpen = function () {
 
 const burgerButtonClose = function () {
   const burgerLines = document.querySelectorAll('.icon-burger__line');
+
   console.log(burgerLines);
   for (let i = 0; i < burgerLines.length; i++) {
     burgerLines[i].style.cssText =
@@ -227,238 +235,3 @@ function resizeHandler() {
 (function init() {
   resizeHandler();
 })();
-
-/////////// PAGE ELEMENT ANIMATIONS /////////
-
-function isInFullView(element) {
-  let bounding = element.getBoundingClientRect();
-
-  if (
-    bounding.top >= 0 &&
-    bounding.left >= 0 &&
-    bounding.right <=
-      (window.innerWidth || document.documentElement.clientWidth) &&
-    bounding.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function isTopInView(element, pos = 0.8) {
-  // console.log(pos);
-  let bounding = element.getBoundingClientRect();
-
-  if (bounding.top <= window.innerHeight * pos) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-/////////// SECONDARY LINKS ANIMATION /////////
-
-let secondaryLinks = document.querySelector('.secondary-links');
-
-window.addEventListener(
-  'scroll',
-  function (event) {
-    let card = document.querySelectorAll('.secondary-card');
-
-    if (isInFullView(secondaryLinks)) {
-      for (let i = 0; i < card.length; i++) {
-        setInterval(() => {
-          card[i].style.transform = 'rotateY(0deg)';
-        }, 200 * i + 200);
-      }
-    }
-  },
-  false
-);
-
-/////////// WHYSSE ANIMATION /////////
-
-let whysse = document.querySelector('.whysse');
-
-let whysseH1 = document.querySelector('.whysse__h1');
-let whysseP = document.querySelector('.whysse__p');
-let whySseEl = [whysseH1, whysseP];
-for (let i = 0; i < whySseEl.length; i++) {
-  whySseEl[i].style.opacity = '0';
-  whySseEl[i].style.top = '4rem';
-}
-
-window.addEventListener(
-  'scroll',
-  function (event) {
-    if (isTopInView(whysse, 0.8)) {
-      for (let i = 0; i < whySseEl.length; i++) {
-        setInterval(() => {
-          whySseEl[i].style.opacity = '1';
-          whySseEl[i].style.top = '0';
-        }, 200 * i + 200);
-      }
-    }
-  },
-  false
-);
-
-/////////// SPECIALITIES ANIMATION /////////
-
-let specialities = document.querySelector('.specialities');
-specialities.style.opacity = '0';
-specialities.style.transform = 'translateY(5rem)';
-
-window.addEventListener(
-  'scroll',
-  function (event) {
-    if (isTopInView(specialities, 0.9)) {
-      setInterval(() => {
-        specialities.style.transform = 'translateY(0)';
-        specialities.style.opacity = '1';
-      }, 300);
-    }
-  },
-  false
-);
-
-/////////// COURSES ANIMATION /////////
-
-let courses = document.querySelector('.courses');
-let coursesH1 = document.querySelector('.course-container__h1');
-let coursesP = document.querySelector('.course-container__p');
-let courseBlocks = document.querySelectorAll('.course-block__item');
-let courseEl = [courses, coursesH1, coursesP, ...courseBlocks];
-for (let i = 0; i < courseEl.length; i++) {
-  courseEl[i].style.opacity = '0';
-  courseEl[i].style.top = '5rem';
-}
-
-window.addEventListener('scroll', function (event) {
-  if (isTopInView(courses, 0.8)) {
-    for (let i = 0; i < courseEl.length; i++) {
-      setInterval(() => {
-        courseEl[i].style.opacity = '1';
-        courseEl[i].style.top = '0';
-      }, 100 * i + 100);
-    }
-  }
-});
-
-/////////// RISE ANIMATION /////////
-
-let sectionRise = document.querySelector('.rise');
-let sectionPic = document.querySelector('.rise__img');
-let sectionH2 = document.querySelector('.rise__text--h2');
-let sectionP = document.querySelector('.rise__text--p');
-
-let sectionRiseEl = [sectionRise, sectionPic, sectionH2, sectionP];
-for (let i = 0; i < sectionRiseEl.length; i++) {
-  if (i < 2) {
-    sectionRiseEl[i].style.opacity = '0';
-  } else {
-    sectionRiseEl[i].style.top = '8rem';
-    sectionRiseEl[i].style.opacity = '0';
-  }
-}
-
-window.addEventListener('scroll', function (event) {
-  if (isTopInView(sectionRise, 0.8)) {
-    for (let i = 0; i < sectionRiseEl.length; i++) {
-      setInterval(() => {
-        if (i < 2) {
-          sectionRiseEl[i].style.opacity = '1';
-        } else {
-          sectionRiseEl[i].style.top = '0';
-          sectionRiseEl[i].style.opacity = '1';
-        }
-      }, 300 * i + 300);
-    }
-  }
-});
-
-/////////// ACADEMIC ANIMATION /////////
-
-let academics = document.querySelector('.academics');
-let academicsH1 = document.querySelector('.academics--h1');
-let academicsBlock = document.querySelector('.academic-block');
-
-let academicsEl = [academics, academicsH1, academicsBlock];
-for (let i = 0; i < academicsEl.length; i++) {
-  if (i == 0) {
-    academicsEl[i].style.opacity = '0';
-  } else {
-    academicsEl[i].style.top = '8rem';
-    academicsEl[i].style.opacity = '0';
-  }
-}
-
-window.addEventListener('scroll', function (event) {
-  if (isTopInView(academics, 0.8)) {
-    for (let i = 0; i < academicsEl.length; i++) {
-      setInterval(function () {
-        if (i == 0) {
-          academicsEl[i].style.opacity = '1';
-        } else {
-          academicsEl[i].style.top = '0';
-          academicsEl[i].style.opacity = '1';
-        }
-      }, 300 * i + 300);
-    }
-  }
-});
-
-/////////// CERTIFICATION ANIMATION /////////
-
-let certification = document.querySelector('.certification');
-let certificationH1 = document.querySelector('.certification--h1');
-let certificationBlock = document.querySelector('.certification-block');
-
-let certificationEl = [certification, certificationH1, certificationBlock];
-for (let i = 0; i < certificationEl.length; i++) {
-  if (i == 0) {
-    certificationEl[i].style.opacity = '0';
-  } else {
-    certificationEl[i].style.top = '8rem';
-    certificationEl[i].style.opacity = '0';
-  }
-}
-
-window.addEventListener('scroll', function (event) {
-  if (isTopInView(certification, 0.8)) {
-    for (let i = 0; i < certificationEl.length; i++) {
-      setInterval(function () {
-        if (i == 0) {
-          certificationEl[i].style.opacity = '1';
-        } else {
-          certificationEl[i].style.top = '0';
-          certificationEl[i].style.opacity = '1';
-        }
-      }, 300 * i + 300);
-    }
-  }
-});
-
-/////////// CLUBS ANIMATION /////////
-
-let clubs = document.querySelector('.clubs');
-let clubsItem = document.querySelectorAll('.clubs-item');
-let clubsFrame = document.querySelectorAll('.clubs-item__frame');
-
-for (let i = 0; i < clubsItem.length; i++) {
-  clubsItem[i].style.top = '6rem';
-  clubsItem[i].style.opacity = '0';
-}
-
-window.addEventListener('scroll', function (event) {
-  if (isInFullView(clubs)) {
-    for (let i = 0; i < clubsItem.length; i++) {
-      setInterval(function () {
-        clubsItem[i].style.top = '0';
-        clubsItem[i].style.opacity = '1';
-      }, 100 * i + 100);
-    }
-  }
-});
